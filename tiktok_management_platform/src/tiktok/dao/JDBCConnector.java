6cd3425b0057709385bfb996e1006f203ffc68f8
Lecty Kim <jpg-png@naver.com>
2022-12-10 21:31:27 +0900

    fix encoding font broken problem

--- tiktok_management_platform/src/tiktok/dao/JDBCConnector.java
+++ tiktok_management_platform/src/tiktok/dao/JDBCConnector.java
@@ -7,22 +7,20 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JDBCConnector {
-	// 占쏙옙트占쏙옙호占쏙옙 占쏙옙占쏙옙 : ex) 占쏙옙占싼댐옙 - (占쏙옙占싻곤옙 21, 占쏙옙占쏙옙占쏙옙 20, ...)
-	// 占쏙옙표占쏙옙화 占쏙옙占쏙옙占쏙옙 URL, 占쏙옙 占쏙옙화占쏙옙호占쏙옙 占쏙옙트占쏙옙호
 	
-	/// 占썩본 占쏙옙占쏙옙 - base configuration
-	private static final String DRIVER_PATH = "com.mysql.cj.jdbc.Driver"; // 占쌨모리울옙 占싸듸옙占쏙옙 占쏙옙 占쌍댐옙.
+	// JDBC Connector - base configuration
+	private static final String DRIVER_PATH = "com.mysql.cj.jdbc.Driver"; // 최신 버전은 cj를 사용함.
 	private static final String URL = "jdbc:mysql://127.0.0.1:3306/tiktok_management?serverTimezone=UTC&useUnicode=yes&characterEncoding=UTF-8";
 	private static final String ID = "root";
-	private static final String PWD = "root"; //root,1234占쌩울옙占쏙옙 占쏙옙占� 占쏙옙占쏙옙
+	private static final String PWD = "root"; //root,1234중 자신의 환경에 맞는 비밀번호를 활용
 	private static Connection con;
 	
 	public static Connection getCon() {
 		try {
 			Class.forName(DRIVER_PATH);
-			System.out.println("�젙�긽�쟻�쑝濡� JDBC Driver媛� Road �릺�뿀�뒿�땲�떎..");
-			con = DriverManager.getConnection(URL, ID, PWD); // 占쏙옙占쏙옙 처占쏙옙占쏙옙 占쌔억옙 占쏙옙占쏙옙占� 占쏙옙 占쌍댐옙.
-			System.out.println("�뿰寃곗씠 �옒 �릺�뿀�뒿�땲�떎.");
+			
+			con = DriverManager.getConnection(URL, ID, PWD); // JDBC와 연결합니다.
+			
 		} catch (ClassNotFoundException e) { 
 			e.printStackTrace();
 		} catch (SQLException e) { 
@@ -35,7 +33,7 @@ public class JDBCConnector {
 		try {
 			Statement stmt = con.createStatement();
 			String sql = "select * from movie";
-			ResultSet rs = stmt.executeQuery(sql);  // sql 占쏙옙占쏙옙占� 占쏙옙占쏙옙占쏙옙占쏙옙 占쌨소드가 占쌕몌옙占쏙옙.
+			ResultSet rs = stmt.executeQuery(sql);  // sql문 활용
 			while (rs.next()) {
 				System.out.print(rs.getInt("movie_id") + "\t");
 				System.out.print(rs.getInt("user") + "\t");
